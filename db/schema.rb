798043a29bdff793dea5cc1f15a75c45ba4dc793
La cholita <julietajaku@gmail.com>
2014-06-29 17:31:56 +0200

    Set myself as admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140628085553) do
+ActiveRecord::Schema.define(version: 20140629152531) do
 
   create_table "paniers", force: true do |t|
     t.date     "semaine"
@@ -38,11 +38,12 @@ ActiveRecord::Schema.define(version: 20140628085553) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",               default: "", null: false
-    t.string   "encrypted_password",  default: "", null: false
+    t.string   "email",               default: "",    null: false
+    t.string   "encrypted_password",  default: "",    null: false
     t.datetime "remember_created_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",               default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
