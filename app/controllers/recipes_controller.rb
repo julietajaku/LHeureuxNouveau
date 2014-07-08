246f96d6d264fc291348e7546cbd4e93bfdd2d7b
La cholita <julietajaku@gmail.com>
2014-07-08 10:22:09 +0200

    Nested forms using gem 'nested_form'
    Works.
    Nice to have: re-define Products for each adtl ingredient, to remove
    already chosen ingredients

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,6 +20,7 @@ class RecipesController < ApplicationController
   # GET /recipes/new
   def new
     @recipe = current_user.recipes.new
+    3.times {@recipe.ingredients.build}
   end
 
   # GET /recipes/1/edit
@@ -87,6 +88,6 @@ class RecipesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def recipe_params
-      params.require(:recipe).permit(:title, :instructions, :short_desc, :panier_id)
+      params.require(:recipe).permit(:title, :instructions, :short_desc, :panier_id, :ingredients_attributes)
     end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,5 @@ class Recipe < ActiveRecord::Base
 	has_and_belongs_to_many :paniers
 	belongs_to :user
 	has_many :ingredients
+	accepts_nested_attributes_for :ingredients, :allow_destroy => true
 end
