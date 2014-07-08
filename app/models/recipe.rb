360b3f7726dc50e610427c26469c99d3e0adc3c8
La cholita <julietajaku@gmail.com>
2014-07-08 10:29:46 +0200

    Revert "Forms to add Ingredients to Recipes"
    This reverts commit a5d0f1ae3cb9233d2e0b6388944048293c6a42cc.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,6 @@
 
 class Recipe < ActiveRecord::Base
 	has_and_belongs_to_many :paniers
-	
 	belongs_to :user
-
 	has_many :ingredients
-	accepts_nested_attributes_for :ingredients, :reject_if => :all_blank, :allow_destroy => true
 end
